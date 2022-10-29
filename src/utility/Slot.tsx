f8b1a96a8f0598921ab60e3177a49596099aea2e
ootidea <ootidea@gmail.com>
2022-10-29 13:25:24 +0900

    fix: DateInput date cell reactivity bug

--- src/utility/Slot.tsx
+++ src/utility/Slot.tsx
@@ -1,12 +1,11 @@
 import { ParentProps } from 'solid-js/types/render/component'
-import { Dynamic } from 'solid-js/web'
 import { SkelSlot } from './props'
 
 export type SlotProps<T> = ParentProps<{ content: SkelSlot<T> | undefined; params: T }>
 
 export function Slot<T>(props: SlotProps<T>) {
   if (props.content instanceof Function) {
-    return <Dynamic component={props.content} {...props.params} />
+    return <>{props.content(props.params)}</>
   } else {
     return <>{props.content ?? props.children}</>
   }
