4027e2cc642efc16a22c406219ef69a9532979e2
ootidea <ootidea@gmail.com>
2022-11-06 08:26:45 +0900

    feat: add Resizable sample

--- dev/page/ResizableComponent.tsx
+++ dev/page/ResizableComponent.tsx
@@ -1,9 +1,12 @@
+import { createSignal } from 'solid-js'
 import { Image } from '../../src/Image'
 import { Resizable } from '../../src/Resizable'
 import { PageTitle } from '../PageTitle'
 import { Sample } from '../Sample'
 
 export function ResizableComponent() {
+  const [widthPx, setWidthPx] = createSignal(500)
+
   return (
     <article>
       <PageTitle>Resizable</PageTitle>
@@ -17,6 +20,16 @@ export function ResizableComponent() {
             width="100%"
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Polar_Bear_ANWR_1.jpg/320px-Polar_Bear_ANWR_1.jpg"
           />
+        </Resizable>
+      </Sample>
+
+      <Sample title="Bind to signal">
+        <Resizable onChangeWidthPx={setWidthPx}>
+          <div
+            style={{ width: `${widthPx()}px`, border: 'gray 1px dashed', padding: '1em', 'box-sizing': 'border-box' }}
+          >
+            widthPx() === {widthPx()}
+          </div>
         </Resizable>
       </Sample>
     </article>
