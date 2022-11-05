30de4515f1f65ebbf46b82287f520b2c976ba87b
ootidea <ootidea@gmail.com>
2022-11-05 15:03:12 +0900

    feat: add Gravity samples

--- dev/page/GravityComponent.tsx
+++ dev/page/GravityComponent.tsx
@@ -7,8 +7,23 @@ export function GravityComponent() {
     <article>
       <PageTitle>Gravity</PageTitle>
       <Sample title="Basic example">
-        <div style={{ width: '20rem', height: '10rem', border: '1px dashed gray' }}>
+        <div style={{ width: '12rem', height: '9rem', border: '1px dashed gray' }}>
           <Gravity>center</Gravity>
+        </div>
+        <div style={{ width: '12rem', height: '9rem', border: '1px dashed gray' }}>
+          <Gravity to="bottom">center</Gravity>
+        </div>
+        <div style={{ width: '12rem', height: '9rem', border: '1px dashed gray' }}>
+          <Gravity to="left">center</Gravity>
+        </div>
+      </Sample>
+
+      <Sample title="Corners">
+        <div style={{ width: '12rem', height: '9rem', border: '1px dashed gray' }}>
+          <Gravity to="top right">center</Gravity>
+        </div>
+        <div style={{ width: '12rem', height: '9rem', border: '1px dashed gray' }}>
+          <Gravity to="bottom left">center</Gravity>
         </div>
       </Sample>
     </article>
