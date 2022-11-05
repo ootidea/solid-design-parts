1e5c0fbf510d7f111fc99e323cc3beb1fc3c6c1e
ootidea <ootidea@gmail.com>
2022-11-05 14:44:13 +0900

    feat: translucent Scrollable thumb

--- dev/page/ScrollableComponent.tsx
+++ dev/page/ScrollableComponent.tsx
@@ -1,4 +1,5 @@
 import { For } from 'solid-js'
+import { Image } from '../../src/Image'
 import { Scrollable } from '../../src/Scrollable'
 import { PageTitle } from '../PageTitle'
 import { Sample } from '../Sample'
@@ -9,9 +10,15 @@ export function ScrollableComponent() {
       <PageTitle>Scrollable</PageTitle>
 
       <Sample title="Basic example">
-        <div style={{ height: '10rem', width: '100%' }}>
+        <div style={{ height: '10rem' }}>
           <Scrollable>
             <For each={[...Array(10).keys()]}>{(i) => <p>{i}</p>}</For>
+          </Scrollable>
+        </div>
+
+        <div style={{ height: '7rem', width: 'max-content' }}>
+          <Scrollable>
+            <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Polar_Bear_ANWR_1.jpg/320px-Polar_Bear_ANWR_1.jpg" />
           </Scrollable>
         </div>
       </Sample>
