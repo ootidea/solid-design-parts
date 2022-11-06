2599f2879b7b673ece058964d10b5ce31163a18a
ootidea <ootidea@gmail.com>
2022-11-06 14:30:02 +0900

    fix: bundle svg files as data URI

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,14 +1,16 @@
-import postcssOKLabFunction from "@csstools/postcss-oklab-function";
-import autoprefixer from "autoprefixer";
-import postcss from "rollup-plugin-postcss";
-import withSolid from "rollup-preset-solid";
+import postcssOKLabFunction from '@csstools/postcss-oklab-function'
+import url from '@rollup/plugin-url'
+import autoprefixer from 'autoprefixer'
+import postcss from 'rollup-plugin-postcss'
+import withSolid from 'rollup-preset-solid'
 
 export default withSolid({
-  input: "src/index.tsx",
-  targets: ["esm", "cjs"],
+  input: 'src/index.tsx',
+  targets: ['esm', 'cjs'],
   plugins: [
     postcss({
       plugins: [autoprefixer(), postcssOKLabFunction({ subFeatures: { displayP3: false } })],
     }),
+    url(),
   ],
-});
+})
