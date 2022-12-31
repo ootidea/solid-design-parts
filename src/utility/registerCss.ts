780eb6234c3816d17c1697b9c49cd89e43177fd1
ootidea <ootidea@gmail.com>
2022-12-31 11:02:06 +0900

    fix: workaround double css injection bug

--- src/utility/registerCss.ts
+++ src/utility/registerCss.ts
@@ -1,9 +1,11 @@
 import css from '../common.scss'
 
 export function registerCss(css: string) {
-  const style = document.createElement('style')
-  style.textContent = css
-  document.head.appendChild(style)
+  // For some reason, just importing a css file doesn't bundle the code to inject it into html.
+  // The built-in injection code is bundled by touching the css text as shown below.
+  // import css from './Button.scss'
+  // registerCss(css: string)
+  css.length
 }
 
 registerCss(css)
