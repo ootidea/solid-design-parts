4969157012b752814d2c416a951c990c50a551cf
ootidea <ootidea@gmail.com>
2023-03-21 22:11:03 +0900

    refactor: simplify Stepper DOM

--- src/Stepper.tsx
+++ src/Stepper.tsx
@@ -1,7 +1,5 @@
 import { rangeTo } from 'base-up'
-import { For } from 'solid-js'
-import { Gravity } from './Gravity'
-import { LayerLayout } from './LayerLayout'
+import { For, Show } from 'solid-js'
 import css from './Stepper.scss'
 import { joinClasses, prepareProps, Props } from './utility/props'
 import { registerCss } from './utility/registerCss'
@@ -19,43 +17,37 @@ export function Stepper(rawProps: StepperProps) {
       style={{ '--solid-design-parts-Stepper_circle-size': '2.5rem' }}
       {...restProps}
     >
-      <LayerLayout style={{ width: '100%' }}>
-        <div
-          class="solid-design-parts-Stepper_line-layer"
-          style={{ 'grid-template-columns': `1fr repeat(${props.titles.length - 1}, 2fr) 1fr` }}
-        >
-          <div />
-          <For each={rangeTo(props.titles.length - 1)}>
-            {(i) => (
-              <div
-                class="solid-design-parts-Stepper_line"
-                classList={{ 'solid-design-parts-Stepper_reached': i < props.currentStep }}
-              />
-            )}
-          </For>
-          <div />
-        </div>
-        <div
-          class="solid-design-parts-Stepper_circle-layer"
-          style={{ 'grid-template-columns': `repeat(${props.titles.length}, 1fr)` }}
-        >
-          <For each={rangeTo(props.titles.length)}>
-            {(i) => (
-              <Gravity>
+      <div
+        class="solid-design-parts-Stepper_diagram"
+        style={{ 'grid-template-columns': `1fr ${props.titles.map(() => 'max-content').join(' 2fr ')} 1fr` }}
+      >
+        {/* dummy element for grid layout */}
+        <div />
+        <For each={rangeTo(props.titles.length)}>
+          {(i) => (
+            <>
+              <Show when={i > 0}>
                 <div
-                  class="solid-design-parts-Stepper_circle"
-                  classList={{
-                    'solid-design-parts-Stepper_reached': i <= props.currentStep,
-                  }}
-                  aria-current={i === props.currentStep ? 'step' : false}
-                >
-                  {i + 1}
-                </div>
-              </Gravity>
-            )}
-          </For>
-        </div>
-      </LayerLayout>
+                  class="solid-design-parts-Stepper_line"
+                  classList={{ 'solid-design-parts-Stepper_reached': i <= props.currentStep }}
+                />
+              </Show>
+              <div
+                class="solid-design-parts-Stepper_circle"
+                classList={{
+                  'solid-design-parts-Stepper_reached': i <= props.currentStep,
+                }}
+                aria-current={i === props.currentStep ? 'step' : false}
+              >
+                {i + 1}
+              </div>
+            </>
+          )}
+        </For>
+        {/* dummy element for grid layout */}
+        <div />
+      </div>
+
       <div
         class="solid-design-parts-Stepper_titles-area"
         style={{ 'grid-template-columns': `repeat(${props.titles.length}, 1fr)` }}
