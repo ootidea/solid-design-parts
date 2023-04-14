5baa4becdbe70dc6eedf2f5446057cef9092c6f8
ootidea <ootidea@gmail.com>
2023-04-14 19:01:13 +0900

    fix: sidebar width bug

--- website/SidebarMenu.tsx
+++ website/SidebarMenu.tsx
@@ -17,8 +17,21 @@ export function SidebarMenu(props: VoidProps<{ componentName: string }>) {
   }
 
   return (
-    <a class={classes.root} href={props.componentName} aria-selected={isActive()} onClick={onClick}>
-      {props.componentName}
-    </a>
+    <div>
+      <a class={classes.link} href={props.componentName} aria-selected={isActive()} onClick={onClick}>
+        {props.componentName}
+      </a>
+      {/*
+      dummy element to prevent width changes caused by bold text.
+      classes.link has padding, so even if you set height: 0 for that element, the height will not become 0.
+      Therefore, enclose it with a div element and set height: 0.
+      https://stackoverflow.com/questions/20827367/why-doesn-t-height-0-hide-my-padded-div-even-with-box-sizing-border-box
+      */}
+      <div class={classes.hiddenSelected}>
+        <a class={classes.link} aria-hidden="true" aria-selected="true">
+          {props.componentName}
+        </a>
+      </div>
+    </div>
   )
 }
