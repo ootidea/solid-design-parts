d3fcadc579a9fec63be8e2186d0491640b59f1f6
ootidea <ootidea@gmail.com>
2023-04-15 12:57:44 +0900

    doc: improve SpeechBubble samples

--- website/page/SpeechBubbleCatalog.tsx
+++ website/page/SpeechBubbleCatalog.tsx
@@ -16,8 +16,8 @@ export const SpeechBubbleCatalog: Catalog = createRoot(() => ({
       title: 'Background color',
       children: (
         <>
-          <SpeechBubble backgroundColor="hsl(0 100% 95%)">Sample</SpeechBubble>
-          <SpeechBubble backgroundColor="#e9ffe9">Sample</SpeechBubble>
+          <SpeechBubble backgroundColor="hsl(0 100% 95%)">Background color: hsl(0 100% 95%)</SpeechBubble>
+          <SpeechBubble backgroundColor="#e9ffe9">Background color: #e9ffe9</SpeechBubble>
         </>
       ),
     },
@@ -25,9 +25,9 @@ export const SpeechBubbleCatalog: Catalog = createRoot(() => ({
       title: 'Radius',
       children: (
         <>
-          <SpeechBubble radius="0">Sample</SpeechBubble>
-          <SpeechBubble radius="999vh">Sample</SpeechBubble>
-          <SpeechBubble radius="8px 0 8px 0">Sample</SpeechBubble>
+          <SpeechBubble radius="0">Radius: 0</SpeechBubble>
+          <SpeechBubble radius="999vh">Radius: 999vh</SpeechBubble>
+          <SpeechBubble radius="8px 0 8px 0">Radius: 8px 0 8px 0</SpeechBubble>
         </>
       ),
     },
@@ -35,9 +35,9 @@ export const SpeechBubbleCatalog: Catalog = createRoot(() => ({
       title: 'Tail width',
       children: (
         <>
-          <SpeechBubble tailWidth="5px">Tail width</SpeechBubble>
-          <SpeechBubble tailWidth="2em">Tail width</SpeechBubble>
-          <SpeechBubble tailWidth="50%">Tail width</SpeechBubble>
+          <SpeechBubble tailWidth="5px">Tail width: 5px</SpeechBubble>
+          <SpeechBubble tailWidth="2em">Tail width: 2em</SpeechBubble>
+          <SpeechBubble tailWidth="50%">Tail width: 50%</SpeechBubble>
         </>
       ),
     },
@@ -45,8 +45,8 @@ export const SpeechBubbleCatalog: Catalog = createRoot(() => ({
       title: 'Tail height',
       children: (
         <>
-          <SpeechBubble tailHeight="5px">Tail height</SpeechBubble>
-          <SpeechBubble tailHeight="1.2em">Tail height</SpeechBubble>
+          <SpeechBubble tailHeight="5px">Tail height: 5px</SpeechBubble>
+          <SpeechBubble tailHeight="1.2em">Tail height: 1.2em</SpeechBubble>
         </>
       ),
     },
@@ -54,8 +54,8 @@ export const SpeechBubbleCatalog: Catalog = createRoot(() => ({
       title: 'Tail offset',
       children: (
         <>
-          <SpeechBubble tailOffsetPercent={80}>Tail offset</SpeechBubble>
-          <SpeechBubble tailOffsetPercent={-30}>Tail offset</SpeechBubble>
+          <SpeechBubble tailOffsetPercent={80}>Tail offset: 80%</SpeechBubble>
+          <SpeechBubble tailOffsetPercent={-30}>Tail offset: -30%</SpeechBubble>
         </>
       ),
     },
@@ -63,8 +63,8 @@ export const SpeechBubbleCatalog: Catalog = createRoot(() => ({
       title: 'Tail skew',
       children: (
         <>
-          <SpeechBubble tailSkew="0.16turn">Tail skew</SpeechBubble>
-          <SpeechBubble tailSkew="-45deg">Tail skew</SpeechBubble>
+          <SpeechBubble tailSkew="0.16turn">Tail skew: 0.16turn</SpeechBubble>
+          <SpeechBubble tailSkew="-45deg">Tail skew: -45deg</SpeechBubble>
         </>
       ),
     },
