890a1b7e1988d499596c5a32d876a4589220719b
ootidea <ootidea@gmail.com>
2023-04-22 17:32:21 +0900

    rework ErrorMessage

--- library/ErrorMessage.tsx
+++ library/ErrorMessage.tsx
@@ -1,14 +1,42 @@
+import { isBoolean, isNullish } from 'base-up'
+import { children, createMemo } from 'solid-js'
+import { AnimatedShow } from './AnimatedShow'
 import './common.scss'
 import './ErrorMessage.scss'
+import { SpeechBubble } from './SpeechBubble'
 import { joinClasses, prepareProps, Props } from './utility/props'
 
 export type ErrorMessageProps = Props<{}, 'p'>
 
 export function ErrorMessage(rawProps: ErrorMessageProps) {
   const [props, restProps] = prepareProps(rawProps, {})
+
+  const childrenMemo = children(() => rawProps.children)
+  const convertTruthy = createMemo(() => {
+    if (isNullish(childrenMemo()) || isBoolean(childrenMemo())) return false
+
+    return childrenMemo()
+  })
+
   return (
-    <p {...restProps} class={joinClasses(rawProps, 'solid-design-parts-ErrorMessage_root')} role="alert">
-      {rawProps.children}
-    </p>
+    <AnimatedShow when={convertTruthy()}>
+      {(content) => (
+        <SpeechBubble
+          {...restProps}
+          class={joinClasses(rawProps, 'solid-design-parts-ErrorMessage_root')}
+          style={{
+            filter: 'drop-shadow(0 0 1px var(--solid-design-parts-error-color));',
+          }}
+          direction="up"
+          tailOffsetPercent={-50}
+          tailHeight="0.3em"
+          tailWidth="0.8em"
+          padding="0.2em 0.4em"
+          role="alert"
+        >
+          {content}
+        </SpeechBubble>
+      )}
+    </AnimatedShow>
   )
 }
