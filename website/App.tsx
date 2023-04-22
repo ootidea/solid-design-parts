282fae4545cef9aebfa77d7d34953de922fce7a6
ootidea <ootidea@gmail.com>
2023-04-22 20:30:54 +0900

    fix: Cloudflare pages redirect bug

--- website/App.tsx
+++ website/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Router, Routes } from '@solidjs/router'
+import { Route, Router, Routes, useNavigate } from '@solidjs/router'
 import { For } from 'solid-js'
 import { Collapsible, Divider, Gravity, Scrollable, Select, StretchLayout } from '../library'
 import { forceCurrentLanguage } from '../library/utility/i18n'
@@ -84,7 +84,14 @@ export function App() {
             <Routes>
               <Route path="components/*" component={ComponentCatalogPage} />
               {/* TODO: Since there is no homepage, a temporary redirect is in place. */}
-              <Route path="/" element={<Navigate href="components/Button" />} />
+              <Route
+                path="/"
+                component={() => {
+                  const navigator = useNavigate()
+                  navigator('components/Button', { resolve: true })
+                  return <></>
+                }}
+              />
             </Routes>
           </main>
         </StretchLayout>
