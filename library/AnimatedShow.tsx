24f24612ab2e0564e24ab15092b412551445c087
ootidea <ootidea@gmail.com>
2023-04-23 10:50:04 +0900

    refactor: inline AnimatedShow function

--- library/AnimatedShow.tsx
+++ library/AnimatedShow.tsx
@@ -26,6 +26,7 @@ export function AnimatedShow<const T>(rawProps: AnimatedShowProps<T>) {
   // Signal variable indicating whether props.children should be present on the DOM.
   const shouldBeInDomSignal = createSignalObject(Boolean(props.when))
 
+  let lastAnimation: Animation | undefined
   // A variable required to render props.children until animation is complete.
   let lastTruthyValue = props.when
   createDeferEffect(
@@ -35,27 +36,25 @@ export function AnimatedShow<const T>(rawProps: AnimatedShowProps<T>) {
         lastTruthyValue = props.when
       }
 
-      changeShown(Boolean(props.when))
+      lastAnimation?.cancel()
+      if (props.when) {
+        shouldBeInDomSignal.value = true
+        lastAnimation = element?.animate(props.animation.keyframes, props.animation.options)
+        lastAnimation?.addEventListener('finish', () => {
+          props.onFinishEnterAnimation?.()
+        })
+      } else {
+        lastAnimation = element?.animate(props.animation.keyframes, {
+          ...props.animation.options,
+          direction: 'reverse',
+        })
+        lastAnimation?.addEventListener('finish', () => {
+          shouldBeInDomSignal.value = false
+          props.onFinishExitAnimation?.()
+        })
+      }
     }
   )
-
-  let lastAnimation: Animation | undefined
-  function changeShown(newWhen: boolean) {
-    lastAnimation?.cancel()
-    if (newWhen) {
-      shouldBeInDomSignal.value = true
-      lastAnimation = element?.animate(props.animation.keyframes, props.animation.options)
-      lastAnimation?.addEventListener('finish', () => {
-        props.onFinishEnterAnimation?.()
-      })
-    } else {
-      lastAnimation = element?.animate(props.animation.keyframes, { ...props.animation.options, direction: 'reverse' })
-      lastAnimation?.addEventListener('finish', () => {
-        shouldBeInDomSignal.value = false
-        props.onFinishExitAnimation?.()
-      })
-    }
-  }
 
   return (
     <div class="solid-design-parts-AnimatedShow_root" ref={element}>
