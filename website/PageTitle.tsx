57745ad21167b48159ee9a7a95f4813a88052d0f
ootidea <ootidea@gmail.com>
2023-04-30 13:58:43 +0900

    feat: use href on GitHub IconButton

--- website/PageTitle.tsx
+++ website/PageTitle.tsx
@@ -5,15 +5,16 @@ import classes from './PageTitle.module.scss'
 export function PageTitle(props: { children: string }) {
   document.title = `${props.children} - solid-design-parts`
 
-  function onClick() {
-    window.open(`https://github.com/ootidea/solid-design-parts/blob/main/website/pages/${props.children}Catalog.tsx`)
-  }
-
   return (
     <h1 class={classes.root}>
       {props.children}
-      {/* TODO: use href instead of onClick. */}
-      <IconButton src={gitHubIcon} size="1.2em" iconSize="70%" iconColor="hsl(0 0% 50%)" onClick={onClick} />
+      <IconButton
+        src={gitHubIcon}
+        href={`https://github.com/ootidea/solid-design-parts/blob/main/website/pages/${props.children}Catalog.tsx`}
+        size="1.2em"
+        iconSize="70%"
+        iconColor="hsl(0 0% 50%)"
+      />
     </h1>
   )
 }
