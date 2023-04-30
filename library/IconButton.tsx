416eb0247a25f891ecf6077d27147ae656d73422
ootidea <ootidea@gmail.com>
2023-04-30 13:55:57 +0900

    feat: add IconButton href prop

--- library/IconButton.tsx
+++ library/IconButton.tsx
@@ -1,3 +1,4 @@
+import { Show } from 'solid-js'
 import { createMemoObject, createSignalObject } from 'solid-signal-object'
 import './common.scss'
 import { Icon } from './Icon'
@@ -6,35 +7,48 @@ import { Spinner } from './Spinner'
 import { calculateActiveColor, calculateHoverColor, CssColor, resolveColorOnBodyElement } from './utility/color'
 import { joinClasses, joinStyle, prepareProps, Props } from './utility/props'
 
-export type IconButtonProps = Props<
-  {
-    src: string
-    size?: string
-    radius?: string
-    iconSize?: string
-    iconColor?: CssColor
-    backgroundColor?: CssColor
-    disabledColor?: CssColor
-    type?: 'submit' | 'button' | 'reset'
-    rotate?: string
-    onClick?: (event: MouseEvent) => unknown
-  },
-  'button'
->
+type CommonOwnProps = {
+  src: string
+  size?: string
+  radius?: string
+  iconSize?: string
+  iconColor?: CssColor
+  disabled?: boolean
+  backgroundColor?: CssColor
+  disabledColor?: CssColor
+  rotate?: string
+  onClick?: (event: MouseEvent) => unknown
+}
+
+export type IconButtonProps =
+  | Props<
+      CommonOwnProps & {
+        href?: never
+        type?: 'submit' | 'button' | 'reset'
+      },
+      'button'
+    >
+  | Props<
+      CommonOwnProps & {
+        href: string
+        type?: string
+      },
+      'a'
+    >
 
 export function IconButton(rawProps: IconButtonProps) {
   const [props, restProps] = prepareProps(
     rawProps,
     {
+      disabled: false,
       size: 'var(--solid-design-parts-IconButton_default-size)',
       radius: 'var(--solid-design-parts-IconButton_default-radius)',
       iconSize: 'var(--solid-design-parts-IconButton_icon-default-size)',
       iconColor: 'var(--solid-design-parts-Icon_default-color)',
       backgroundColor: 'transparent',
       disabledColor: 'var(--solid-design-parts-IconButton_disabled-default-color)',
-      type: 'button',
     },
-    ['src', 'disabled', 'rotate', 'onClick']
+    ['src', 'type', 'rotate', 'onClick']
   )
 
   const backgroundColor = createMemoObject(() => resolveColorOnBodyElement(props.backgroundColor))
@@ -50,7 +64,21 @@ export function IconButton(rawProps: IconButtonProps) {
     }
   }
 
-  return (
+  const content = (
+    <Show
+      when={!isInProgress.value}
+      fallback={<Spinner size="65%" color={props.disabled ? props.disabledColor : props.iconColor} />}
+    >
+      <Icon
+        src={props.src}
+        size={props.iconSize}
+        color={props.disabled ? props.disabledColor : props.iconColor}
+        rotate={props.rotate}
+      />
+    </Show>
+  )
+
+  return typeof rawProps.href !== 'string' ? (
     <button
       {...restProps}
       class={joinClasses(rawProps, 'solid-design-parts-IconButton_root')}
@@ -62,21 +90,30 @@ export function IconButton(rawProps: IconButtonProps) {
         '--solid-design-parts-IconButton_background-hover-color': calculateHoverColor(backgroundColor.value),
         '--solid-design-parts-IconButton_background-active-color': calculateActiveColor(backgroundColor.value),
       })}
-      type={props.type}
-      disabled={props.disabled}
+      type={rawProps.type ?? 'button'}
+      disabled={props.disabled || isInProgress.value}
       aria-disabled={props.disabled}
       onClick={onClick}
     >
-      {isInProgress.value ? (
-        <Spinner size="65%" color={props.disabled ? props.disabledColor : props.iconColor} />
-      ) : (
-        <Icon
-          src={props.src}
-          size={props.iconSize}
-          color={props.disabled ? props.disabledColor : props.iconColor}
-          rotate={props.rotate}
-        />
-      )}
+      {content}
     </button>
+  ) : (
+    <a
+      {...restProps}
+      class={joinClasses(rawProps, 'solid-design-parts-IconButton_root')}
+      style={joinStyle(rawProps.style, {
+        '--solid-design-parts-IconButton_size': props.size,
+        '--solid-design-parts-IconButton_radius': props.radius,
+        '--solid-design-parts-IconButton_icon-size': props.iconSize,
+        '--solid-design-parts-IconButton_background-color': backgroundColor.value,
+        '--solid-design-parts-IconButton_background-hover-color': calculateHoverColor(backgroundColor.value),
+        '--solid-design-parts-IconButton_background-active-color': calculateActiveColor(backgroundColor.value),
+      })}
+      type={props.type}
+      aria-disabled={props.disabled}
+      onClick={onClick}
+    >
+      {content}
+    </a>
   )
 }
