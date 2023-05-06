a87efa8cb40b284b2afce4ee1dd58a4f171331a4
ootidea <ootidea@gmail.com>
2023-05-06 15:10:17 +0900

    style: ProgressBar transition

--- website/pages/ProgressBarCatalog.tsx
+++ website/pages/ProgressBarCatalog.tsx
@@ -1,6 +1,9 @@
 import { createRoot } from 'solid-js'
 import { Catalog } from './ComponentCatalogPage'
 import { ProgressBar } from '../../library/ProgressBar'
+import { Const } from '../../library/Const'
+import { createSignalObject } from 'solid-signal-object'
+import { Button } from '../../library'
 
 export const ProgressBarCatalog: Catalog = createRoot(() => ({
   samples: [
@@ -12,6 +15,24 @@ export const ProgressBarCatalog: Catalog = createRoot(() => ({
           <ProgressBar value={0.5} />
           <ProgressBar value={1} />
         </>
+      ),
+    },
+    {
+      title: (
+        <>
+          Binding <code>value</code> to a signal
+        </>
+      ),
+      children: (
+        <Const value={createSignalObject(0.5)}>
+          {(valueSignal) => (
+            <>
+              <ProgressBar value={valueSignal.value} />
+              <Button.ghost onClick={() => (valueSignal.value = 0.25)}>value = 0.25</Button.ghost>
+              <Button.ghost onClick={() => (valueSignal.value = 0.75)}>value = 0.75</Button.ghost>
+            </>
+          )}
+        </Const>
       ),
     },
   ],
